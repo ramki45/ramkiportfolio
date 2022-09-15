db0dd04459ce1c33197b2b6c3a06275e1870b982
Ramki <89958455+ramki45@users.noreply.github.com>
2022-09-15 18:32:29 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Ramki Marichamy',
     title: "MERN STACK Developer",
     desciption:"Looking forward to product based as well as startup's where i will develope myself and have a self growth in IT sector. I am very keen to work for the IT industry. ",
-    image: 'https://avatars.githubusercontent.com/u/89958455?s=400&u=46012a10bb8b0c2862f8895d9aaa940fc3b45797&v=4',
+    image: 'https://github.com/ramki45/ramkiportfolio/blob/main/src/assets/20220421_104344%20(1).jpg',
     resumePdf: "https://drive.google.com/file/d/1OQOnBT3S2Q02d1V84WHOOjr0dQcF4K3d/view?usp=sharing"
 }
